f3c84e7a9e680f091b3a8245b08951130146b4fc
Rohan Kulakarni <70747964+theRawHawn@users.noreply.github.com>
2026-04-20 16:11:01 +0530

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -132,7 +132,7 @@ export default function Footer() {
               </li>
               <li className="flex items-center gap-2">
                 <svg className="w-4 h-4 flex-shrink-0" fill="currentColor" viewBox="0 0 20 20"><path fillRule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clipRule="evenodd" /></svg>
-                <span>GSTIN: 29AAJFB1234A1Z5</span>
+                <span>GSTIN: </span>
               </li>
             </ul>
           </div>
